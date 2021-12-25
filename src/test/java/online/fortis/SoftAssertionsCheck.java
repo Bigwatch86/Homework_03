b68bfab8caf0ad92067ab15daa6f5d82c9745b58
Bigwatch86 <bigwatch86@gmail.com>
2021-12-25 16:05:27 +0300

    added checks

--- src/test/java/online/fortis/SoftAssertionsCheck.java
+++ src/test/java/online/fortis/SoftAssertionsCheck.java
@@ -2,8 +2,10 @@ package online.fortis;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static java.lang.Thread.sleep;
 
 public class SoftAssertionsCheck {
     @Test
@@ -12,12 +14,13 @@ public class SoftAssertionsCheck {
         // открыть страницу Selenide в Github
         open("https://github.com/selenide/selenide");
         // перейти в раздел Wiki проекта
-        $("data-content=\"Wiki\"").click();
-
+        $("#wiki-tab").click();
         // убедиться, что в списке страниц (Pages) есть страница SoftAssertions
-
-        // откройть страницу SoftAssertions, проверить что внутри есть пример кода для JUnit5
-
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("[data-filterable-for=\"wiki-pages-filter\"]").shouldHave(text("SoftAssertions"));
+        // открыть страницу SoftAssertions, проверить что внутри есть пример кода для JUnit5
+        $("[data-filterable-for=\"wiki-pages-filter\"]").click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 
 }
